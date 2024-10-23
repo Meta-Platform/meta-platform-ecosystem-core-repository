926779aca2f958546dacd948a822fef8c09be996
Kaio Cezar <kadisk.shark@gmail.com>
2024-10-22 21:01:24 -0300

    rename daemo.sock para instance-manager.sock

--- Main.Module/Application.layer/ecosystem-daemon-launcher.cli/src/Commands/DaemonLaunch.command.js
+++ Main.Module/Application.layer/ecosystem-daemon-launcher.cli/src/Commands/DaemonLaunch.command.js
@@ -5,7 +5,7 @@ const FormatterDataLog = require("../Utils/FormatterDataLog")
 
 const DaemonLaunchCommand = async () => {
 
-	process.env.DAEMON_SOCKET_PATH = "/home/kaiocezar/Workspaces/meta-platform-repo/EcosystemData/sockets/daemon.sock"
+	process.env.DAEMON_SOCKET_PATH = "/home/kaiocezar/Workspaces/meta-platform-repo/EcosystemData/sockets/instance-manager.sock"
 
 	const loggerEmitter = new EventEmitter()
 	loggerEmitter.on("log", async (dataLog) => console.log(await FormatterDataLog(dataLog)))
