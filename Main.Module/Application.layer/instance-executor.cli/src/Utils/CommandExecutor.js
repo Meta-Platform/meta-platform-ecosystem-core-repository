8c2c589442cbe3ba4db428576461aaba16e50b2c
Kaio Cezar <kadisk.shark@gmail.com>
2024-11-05 02:27:57 -0300

    correção command Executor

--- Main.Module/Application.layer/instance-executor.cli/src/Utils/CommandExecutor.js
+++ Main.Module/Application.layer/instance-executor.cli/src/Utils/CommandExecutor.js
@@ -52,7 +52,7 @@ const CommandExecutor = async ({
         })
         loggerEmitter 
         && loggerEmitter.emit("log", {sourceName: "CommandExecutor", type:"success", message:"Conectado ao Ecosystem Daemon!"})
-        CommandFunction({APIs}, loggerEmitter)
+        await CommandFunction({APIs}, loggerEmitter)
     } catch(e){
    
         if(e.erroredSysCall === "connect"){
