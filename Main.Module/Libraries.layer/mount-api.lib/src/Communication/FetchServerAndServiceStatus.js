741daaa78d661b392a51c54f0bcca1f242730929
Kaio Cezar <kadisk.shark@gmail.com>
2024-11-15 21:14:02 -0300

    ajuste fetch

--- Main.Module/Libraries.layer/mount-api.lib/src/Communication/FetchServerAndServiceStatus.js
+++ Main.Module/Libraries.layer/mount-api.lib/src/Communication/FetchServerAndServiceStatus.js
@@ -3,21 +3,35 @@ const http = require('http')
 const FetchServerAndServiceStatus = async ({
     serverResourceEndpointPath,
     mainApplicationSocketPath
-}) => {
+}) => new Promise((resolve, reject) => {
     
-    const agent = new http.Agent({ socketPath: mainApplicationSocketPath})
-    const url = `http://localhost${serverResourceEndpointPath}`
     const options = {
-        agent,
+        socketPath: mainApplicationSocketPath,
+        path: serverResourceEndpointPath,
         method: 'GET'
     }
 
-    const response = await fetch(url, options)
-    if (!response.ok) {
-        throw response
-    }
-    const data = await response.json()
-    return data
-}
+    const req = http.request(options, (res) => {
+
+        let data = ''
+        res.on('data', (chunk) => {
+            data += chunk
+        })
+
+        res.on('end', () => {
+
+            if (res.statusCode >= 200 && res.statusCode < 300)
+                resolve(JSON.parse(data))
+            else
+                reject(new Error(`HTTP status ${res.statusCode}: ${data}`))
+
+        })
+
+    })
+
+    req.on('error', (err) => reject(err))
+
+    req.end()
+})
 
 module.exports = FetchServerAndServiceStatus
--- Main.Module/Libraries.layer/mount-api.lib/src/Communication/GetRequest.js
+++ Main.Module/Libraries.layer/mount-api.lib/src/Communication/GetRequest.js
@@ -1,14 +1,15 @@
 const querystring = require('querystring')
 const http = require('http')
 
-const getURLPath = (path, parameters) => 
-parameters && parameters.length > 0
-? parameters
-    .filter((parameter) => (parameter.in == "path"))
-    .reduce((path, parameter) => path.replace(`:${parameter.name}`, parameter.value), path)
-: path
+const GetURLPath = (path, parameters) => 
+    parameters && parameters.length > 0
+        ? parameters
+            .filter((parameter) => (parameter.in == "path"))
+            .reduce((path, parameter) => path.replace(`:${parameter.name}`, parameter.value), path)
+        : path
 
-const getURLQuery = (path, parameters) => {
+const GetURLQuery = (path, parameters) => {
+
     const newParameters = parameters && parameters
     .filter((parameter) => (parameter.in == "query" && parameter.value && parameter.value !== ""))
 
@@ -22,18 +23,20 @@ const getURLQuery = (path, parameters) => {
         return `${path}?${querystring.stringify(values, null)}`
     }else
         return path
+
 }
 
-const getURL = (path, parameters) => {
-    return getURLQuery(getURLPath(path, parameters), parameters)
-}
+const GetURL = (path, parameters) => 
+    GetURLQuery(GetURLPath(path, parameters), parameters)
 
-const getParametersWithData = (parameters, data) => {
+const GetParametersWithData = (parameters, data) => {
+
     return parameters && parameters.map((parameter)=>{
         if(data[parameter.name] !== undefined)
             parameter.value = data[parameter.name]
         return parameter
     })
+
 }
 
 const GetRequest = ({ 
@@ -43,29 +46,49 @@ const GetRequest = ({
     parameters
 }) => {
     return async (data) => {
-        const agent = new http.Agent({ socketPath })
+
         const immutableParameters = parameters && [...parameters.map(item => ({...item}))]
-        const parametersWithData = getParametersWithData(immutableParameters, data)
+
+        const parametersWithData = GetParametersWithData(immutableParameters, data)
+
         const bodyValues = parametersWithData && parametersWithData
-        .filter((parameter) => (parameter.in == "body"))
-        .reduce((bodyValues, {name, value})=>{
-            bodyValues[name] = value
-            return bodyValues
-        }, {})
-        const url = `http://localhost${getURL(path, parametersWithData)}`;
-        const options = {
-            agent,
-            method: method.toLowerCase(),
-            headers: { 'Content-Type': 'application/json' },
-            body: method.toLowerCase() !== "get"
-                    ? JSON.stringify(method.toLowerCase() === "delete" ? ({data:bodyValues}) : bodyValues)
-                    : undefined
-        }
-        const response = await fetch(url, options)
-        if (!response.ok) {
-            throw response
-        }
-        return await response.json()
+            .filter((parameter) => (parameter.in == "body"))
+            .reduce((bodyValues, {name, value})=>{
+                bodyValues[name] = value
+                return bodyValues
+            }, {})
+
+        const url = GetURL(path, parametersWithData)
+        
+        return new Promise((resolve, reject) => {
+            const options = {
+                socketPath,
+                path: url,
+                method: method.toUpperCase(),
+                headers: { 'Content-Type': 'application/json' }
+            }
+            
+            const req = http.request(options, (res) => {
+                let data = ''
+                res.on('data', (chunk) => {
+                    data += chunk
+                })
+                res.on('end', () => {
+                    if (res.statusCode >= 200 && res.statusCode < 300) 
+                        resolve(JSON.parse(data))
+                    else
+                        reject(new Error(`HTTP status ${res.statusCode}: ${data}`))
+                })
+            })
+            
+            req.on('error', (err) => reject(err))
+
+            if (method.toLowerCase() !== 'get') 
+                req.write(JSON.stringify(bodyValues))
+
+            req.end()
+
+        })
     }
 }
 
