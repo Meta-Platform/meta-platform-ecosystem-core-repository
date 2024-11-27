9b550d2a696559ec75a68677c5c8e33532dd6d3f
Kaio Cezar <kadisk.shark@gmail.com>
2024-11-26 22:49:23 -0300

    task item

--- Main.Module/Application.layer/SupervisorControlDashboard.group/supervisor-control-dashboard.webgui/src/Components/TaskItem.tsx
+++ Main.Module/Application.layer/SupervisorControlDashboard.group/supervisor-control-dashboard.webgui/src/Components/TaskItem.tsx
@@ -1,21 +1,24 @@
 import * as React from "react"
-
-import { Label, Segment, Header } from 'semantic-ui-react'
-
+import { Label, Segment } from "semantic-ui-react"
 import styled from "styled-components"
 
 const SegmentTaskStyled = styled(Segment)`
-    padding-left: 10px!important;
-    padding-top: 5px!important;
-    padding-bottom: 5px!important;
-    margin: 5px!important;
-    box-shadow: 1px 1px 4px 0 rgb(34 36 38 / 71%)!important;
+    padding-left: 10px !important;
+    padding-top: 5px !important;
+    padding-bottom: 5px !important;
+    margin: 10px !important;
+    box-shadow: 1px 1px 4px 0 rgb(34 36 38 / 71%) !important;
     cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     &:hover {
-        background-color: antiquewhite!important;
-        border: 1px solid rgb(34 36 38 / 66%)!important;
+        background-color: antiquewhite !important;
+        border: 1px solid rgb(34 36 38 / 66%) !important;
     }
 `
+
+
 const GetColorByStatus = (status) => {
 	switch(status){
 		case "ACTIVE":
@@ -31,24 +34,51 @@ const GetColorByStatus = (status) => {
 	}
 }
 
-const TaskItem = ({
-	index,
-	task,
-	onShowTaskDetails
+const RenderItemTitle = ({
+	objectLoaderType,
+	staticParameters
 }) => {
+	switch(objectLoaderType){
+		case "install-nodejs-package-dependencies":
+		case "application-instance":
+			return <span style={{color:"#100085"}}> | <strong>namespace</strong> {staticParameters.namespace}</span>
+		case "service-instance":
+		case "nodejs-package":
+			return <span style={{color:"#100085"}}> | <strong>tag</strong> {staticParameters.tag}</span>
+		case "endpoint-instance":
+			return <span style={{color:"#100085"}}> | <strong>url</strong> {staticParameters.url}</span>
+	}
+}
 
-	return <SegmentTaskStyled onClick={() => onShowTaskDetails(task.taskId)} style={index % 2===1 ? {backgroundColor: "#f4f4f4"} : {}}>
-				<strong>Task ID {task.taskId}</strong> | {task.objectLoaderType}{" "}
-				{task.pTaskId && <Label style={{marginRight: "5px"}}>Parent Task ID {task.pTaskId}</Label>}
-				<Label size="mini" color={GetColorByStatus(task.status)} style={{marginRight: "5px"}}>{task.status}</Label>
-				{/*
-					Object.keys(task.staticParameters)
-					.map((paramName) => {
-						return <Label size="mini" style={{marginRight: "5px"}}>{paramName}</Label>
-					})
-				*/}
-				
-			</SegmentTaskStyled>
+
+const TaskInfoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const TaskItem = ({
+    index,
+    task,
+    onShowTaskDetails
+}) => {
+    return (
+        <SegmentTaskStyled onClick={() => onShowTaskDetails(task.taskId)} style={{ backgroundColor: "#f4f4f4" }}>
+            <div>
+                <span style={{ fontSize: "1.2rem" }}>
+                    <strong> TID {task.taskId}</strong>
+                    {task.pTaskId && <> | <i>PTID {task.pTaskId}</i></>}
+					<RenderItemTitle
+						objectLoaderType={task.objectLoaderType}
+						staticParameters={task.staticParameters}
+						/>
+				</span>
+            </div>
+			<div>
+				<Label color="grey">{task.objectLoaderType}</Label>
+				<Label color={GetColorByStatus(task.status)}>{task.status}</Label>
+			</div>
+        </SegmentTaskStyled>
+    )
 }
 
 export default TaskItem
