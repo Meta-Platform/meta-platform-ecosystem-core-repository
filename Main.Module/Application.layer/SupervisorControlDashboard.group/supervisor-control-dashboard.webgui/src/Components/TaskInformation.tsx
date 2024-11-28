b37a23c89b47970d4bc4694ed156959b2bcf8114
Kaio Cezar <kadisk.shark@gmail.com>
2024-11-28 02:17:20 -0300

    RenderValue

--- Main.Module/Application.layer/SupervisorControlDashboard.group/supervisor-control-dashboard.webgui/src/Components/TaskInformation.tsx
+++ Main.Module/Application.layer/SupervisorControlDashboard.group/supervisor-control-dashboard.webgui/src/Components/TaskInformation.tsx
@@ -5,6 +5,11 @@ import {
 	Header,
 	Label,
 	Divider,
+    ListItem,
+    ListHeader,
+    ListDescription,
+    ListContent,
+    List
  } from "semantic-ui-react"
 
 
@@ -23,10 +28,44 @@ const GetColorByStatus = (status) => {
 	}
 }
 
+const RenderValue = ({value}) => {
+
+    if(!value) return "UNDEFINED"
+
+    if(typeof value === "string")
+        return <div style={{ marginLeft:"10px", color:"rgb(98 98 98)", backgroundColor: "#e8e8e8", padding:"10px" }}>{value}</div>
+
+    if(Array.isArray(value))
+        return  <List divided relaxed style={{ margin:"0px 15px 0px 15px", backgroundColor: "#e8e8e8", color:"rgb(98 98 98)", padding:"10px" }}>
+                    {
+                        Object.keys(value)
+                        .map((property) => 
+                            <ListItem>
+                                <ListContent>{value[property]}</ListContent>
+                            </ListItem>)
+                    }
+                </List>
+
+    if(typeof value === "object")
+        return  <List divided relaxed style={{ margin:"0px 15px 0px 15px", backgroundColor: "#e8e8e8", color:"rgb(98 98 98)", padding:"10px" }}>
+                    {
+                        Object.keys(value)
+                        .map((property) => 
+                            <ListItem>
+                                <ListContent>
+                                    <ListHeader style={{ color:"rgb(98 98 98)" }}>{property}</ListHeader>
+                                    <ListDescription style={{ color:"rgb(98 98 98)" }}>{value[property]}</ListDescription>
+                                </ListContent>
+                            </ListItem>)
+                    }
+                </List>
+    
+
+}
+
 const TaskInformation = ({
 	taskInformation
 }:any) => {
-
 
 	return <Segment style={{backgroundColor: "#f4f4f4"}}>
                 <Header as='h2' textAlign='center'>
@@ -49,8 +88,8 @@ const TaskInformation = ({
                             Object.keys(taskInformation.staticParameters)
                             .map((paramName, key) =>
                             <div key={key} style={{marginBottom:"10px"}}>
-                                <strong>{paramName}</strong><br/>
-                                <span style={{marginLeft:"10px",color:"rgb(98 98 98)"}}>{taskInformation.staticParameters[paramName]}</span>
+                                <strong>{paramName}</strong>
+                                <RenderValue value={taskInformation.staticParameters[paramName]}/>
                             </div>)
                         }
                 </Segment>
