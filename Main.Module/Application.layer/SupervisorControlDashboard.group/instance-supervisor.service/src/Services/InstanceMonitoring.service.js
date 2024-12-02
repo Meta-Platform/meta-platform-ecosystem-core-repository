e9c29b619902bc40d141ebb242eb64119861bd4b
Kaio Cezar <kadisk.shark@gmail.com>
2024-12-02 18:17:23 -0300

    AreArraysEqual e remoção do interval

--- Main.Module/Application.layer/SupervisorControlDashboard.group/instance-supervisor.service/src/Services/InstanceMonitoring.service.js
+++ Main.Module/Application.layer/SupervisorControlDashboard.group/instance-supervisor.service/src/Services/InstanceMonitoring.service.js
@@ -1,26 +1,8 @@
 const EventEmitter = require('node:events')
 
-const UPDATE_CHECK_INTERVAL_TIMESTAMP = 2000
-    
+const AreArraysEqual = require("../Utils/AreArraysEqual")
+
 const SOCKET_FILE_LIST_CHANGE_EVENT = Symbol()    
-
-const CopyArray = (array) => [...array]
-
-const AreArraysEqual = (array1, array2) => {
-
-    if (array1.length !== array2.length) 
-        return false
-
-    const sortedArray1 = CopyArray(array1).sort()
-    const sortedArray2 = CopyArray(array2).sort()
-
-    for (let i = 0; i < sortedArray1.length; i++) 
-        if (sortedArray1[i] !== sortedArray2[i]) 
-            return false
-        
-    return true
-
-}
 
 const InstanceMonitoringService = (params) => {
 
@@ -30,39 +12,26 @@ const InstanceMonitoringService = (params) => {
         onReady 
     } = params
 
-    const ListSocketFilesName = supervisorLib.require("ListSocketFilesName")
-
+    
+    const WatchSocketDirectory = supervisorLib.require("WatchSocketDirectory")
+    
     const eventEmitter = new EventEmitter()
 
     let socketFileNameList = []
-    let intervalId = null
 
-    const _StartCheckUpdate = () => {
-        if (!intervalId) {
-            intervalId = setInterval(_UpdateSocketFileListName, UPDATE_CHECK_INTERVAL_TIMESTAMP)
-        }
-    }
-
-    const _StopCheckUpdate = () => {
-        if (intervalId) {
-            clearInterval(intervalId)
-            intervalId = null
-        }
-    }
-
-    const _UpdateSocketFileListName = async () => {
-        const newSocketFileNameList = await ListSocketFilesName(supervisorSocketsDirPath)
-
-        if(!AreArraysEqual(newSocketFileNameList, socketFileNameList)){
-            socketFileNameList = newSocketFileNameList
-            _NotifySocketFileListChange()
-        }
+    const _StartSocketsDirectoryWatcher = () => {
+        WatchSocketDirectory({
+            directoryPath: supervisorSocketsDirPath, onChangeSocketFileList: (newSocketFileNameList) => {
+                if(!AreArraysEqual(newSocketFileNameList, socketFileNameList)){
+                    socketFileNameList = newSocketFileNameList
+                    _NotifySocketFileListChange()
+                }
+            }})
     }
 
     const _Start = async () => {
-        await _UpdateSocketFileListName()
+        _StartSocketsDirectoryWatcher()
         onReady()
-        _StartCheckUpdate()
     }
     const _NotifySocketFileListChange = () => 
             eventEmitter.emit(SOCKET_FILE_LIST_CHANGE_EVENT, socketFileNameList)
@@ -74,11 +43,6 @@ const InstanceMonitoringService = (params) => {
     const monitoringObject = {
         AddChangeSocketListListener
     }
-    
-    const finalizationRegistry = 
-    new FinalizationRegistry(() => _StopCheckUpdate())
-
-    finalizationRegistry.register(monitoringObject, null)
         
     _Start()
         
