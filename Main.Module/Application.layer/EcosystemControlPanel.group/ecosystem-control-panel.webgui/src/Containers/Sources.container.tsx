c572eb8bcb186f45df332ad8ffc71aac0f7a7e25
Kaio Cezar <kadisk.shark@gmail.com>
2024-12-08 04:12:00 -0300

    SourcesController

--- Main.Module/Application.layer/EcosystemControlPanel.group/ecosystem-control-panel.webgui/src/Containers/Sources.container.tsx
+++ Main.Module/Application.layer/EcosystemControlPanel.group/ecosystem-control-panel.webgui/src/Containers/Sources.container.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react"
 import {
     Table,
     Input,
-    Button,
     Loader,
     Segment
 } from "semantic-ui-react"
@@ -18,12 +17,9 @@ type RepositoryType = {
 
 const SourcesContainer = ({ serverManagerInformation }:any) => {
 
-    const [ repos, setRepos ] = useState<RepositoryType[]>([])
-    const [newNamespace, setNewNamespace] = useState("")
-    const [newPath, setNewPath] = useState("")
+    const [ sourceList, setSourceList ] = useState<RepositoryType[]>([])
     const [ isLoading, setIsLoading ] = useState(true)
 
-    const isButtonRegisterDisable = () => !(newNamespace && newPath)
 
     const _GetSourcesAPI = () => 
         GetAPI({ 
@@ -32,15 +28,15 @@ const SourcesContainer = ({ serverManagerInformation }:any) => {
         })
     
     useEffect(() => {
-        fetchRepoList()
+        fetchSourceList()
     }, [])
     
-    const fetchRepoList = async () => {
+    const fetchSourceList = async () => {
         try {
             const api = _GetSourcesAPI()
-            const response = await api.ListRepositories()
-            const repos = response.data
-            setRepos(repos)
+            const response = await api.ListSources()
+            const sourceList = response.data
+            setSourceList(sourceList)
             setIsLoading(false)
 
         }catch(e){
@@ -48,26 +44,6 @@ const SourcesContainer = ({ serverManagerInformation }:any) => {
         }
     }
 
-    const RegisterRepo = async () => {
-        try {
-            setIsLoading(true)
-            const api = _GetSourcesAPI()
-            const response = await api.RegisterRepository({
-                namespace: newNamespace, 
-                path: newPath
-            })
-
-            setNewNamespace("")
-            setNewPath("")
-
-            fetchRepoList()
-
-        }catch(e){
-            console.log(e)
-        }
-    }
-
-    const handleRegister = () => RegisterRepo()
 
     return <Segment style={{margin:"1em"}}>
                 {
@@ -76,46 +52,24 @@ const SourcesContainer = ({ serverManagerInformation }:any) => {
                     :<Table celled striped>
                         <Table.Header>
                             <Table.Row>
-                                <Table.HeaderCell>namespace</Table.HeaderCell>
+                                <Table.HeaderCell>repositoryNamespace</Table.HeaderCell>
+                                <Table.HeaderCell>sourceType</Table.HeaderCell>
+                                <Table.HeaderCell>repositoryName</Table.HeaderCell>
+                                <Table.HeaderCell>repositoryOwner</Table.HeaderCell>
                                 <Table.HeaderCell>path</Table.HeaderCell>
-                                <Table.HeaderCell/>
+                                <Table.HeaderCell>fileId</Table.HeaderCell>
                             </Table.Row>
                         </Table.Header>
                         <Table.Body>
                             {
-                                repos.map((repo: any, key:number) =>
+                                sourceList.map((repo: any, key:number) =>
                                     <Table.Row key={key}>
                                         {
-                                            ["namespace", "path"]
+                                            ["repositoryNamespace", "sourceType", "repositoryName", "repositoryOwner", "path", "fileId"]
                                                 .map((property, key2) => <Table.Cell key={key2}>{repo[property]}</Table.Cell>)
                                         }
                                     </Table.Row>)
                             }
-                            <Table.Row>
-                                <Table.Cell>
-                                    <Input
-                                        fluid
-                                        placeholder="namespace"
-                                        value={newNamespace}
-                                        onChange={({target:{value}}) => setNewNamespace(value)} />
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Input
-                                        fluid
-                                        placeholder="path"
-                                        value={newPath}
-                                        onChange={({target:{value}}) => setNewPath(value)} />
-                                </Table.Cell>
-                                <Table.Cell style={{textAlign: "center"}}>
-                                        <Button
-                                            disabled = {isButtonRegisterDisable()}
-                                            color    = "blue"
-                                            icon     = "plus"
-                                            onClick  = {handleRegister}>
-                                            register
-                                        </Button>
-                                    </Table.Cell>
-                            </Table.Row>
                         </Table.Body>
                     </Table>
                 }
--- Main.Module/Application.layer/EcosystemControlPanel.group/ecosystem-control-panel.webservice/src/Controllers/Sources.controller.js
+++ Main.Module/Application.layer/EcosystemControlPanel.group/ecosystem-control-panel.webservice/src/Controllers/Sources.controller.js
@@ -1,5 +1,48 @@
-const SourcesController = () => {
-    const ListSources = () => {}
+const path = require("path")
+
+const ExtractSourceListBySourcesData = (sourcesData) => {
+
+    const repositoryNamespaceList = Object.keys(sourcesData)
+    const sourceList = repositoryNamespaceList
+    .reduce((acc, repositoryNamespace) => {
+        return [
+            ...acc,
+            ...sourcesData[repositoryNamespace]
+                .map((sourceData) => ({
+                    repositoryNamespace,
+                    ...sourceData
+                }))
+        ]
+    }, [])
+
+    return sourceList
+    
+}
+
+const SourcesController = (params) => {
+
+
+    const { 
+        installDataDirPath,
+        ecosystemDefaultsFileRelativePath,
+        jsonFileUtilitiesLib 
+    } = params
+
+    const ReadJsonFile = jsonFileUtilitiesLib.require("ReadJsonFile")
+
+    const _GetSourcesData = async () => {
+        const ecosystemDefaultFilePath = path.resolve(installDataDirPath, ecosystemDefaultsFileRelativePath)
+        const ecosystemDefaults = await ReadJsonFile(ecosystemDefaultFilePath)
+        const sourcesDataFilePath = path.resolve(installDataDirPath, ecosystemDefaults.REPOS_CONF_FILENAME_SOURCE_DATA)
+        const sourcesData = await ReadJsonFile(sourcesDataFilePath)
+        return sourcesData
+    }
+
+    const ListSources = async () => {
+        const sourcesData = await _GetSourcesData()
+        const sourceList = ExtractSourceListBySourcesData(sourcesData)
+        return sourceList
+    }
 
     return {
         controllerName : "SourcesController",
