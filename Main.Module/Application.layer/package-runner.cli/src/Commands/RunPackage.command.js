7e056dbffecc34cf72ad974251e1ed05e74516bc
Kaio Cezar <kadisk.shark@gmail.com>
2025-09-12 15:33:55 -0300

    RunPackageCommand partial

--- Main.Module/Application.layer/package-runner.cli/src/Commands/RunPackage.command.js
+++ Main.Module/Application.layer/package-runner.cli/src/Commands/RunPackage.command.js
@@ -3,6 +3,7 @@ const { resolve } = require("path")
 const RunPackageCommand = async ({ args, startupParams, params }) => {
     const { packagePath } = args
     const absolutePackagePath = resolve(process.cwd(), packagePath)
+    console.log("Running package at path:")
     console.log(absolutePackagePath)
 }
 
