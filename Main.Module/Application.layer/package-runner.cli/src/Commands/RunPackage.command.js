d2613621f50cc4aa6d7e3832ce365bfaf764a75c
Kaio Cezar <kadisk.shark@gmail.com>
2025-09-12 12:38:24 -0300

    package-runner

--- Main.Module/Application.layer/package-runner.cli/src/Commands/RunPackage.command.js
+++ Main.Module/Application.layer/package-runner.cli/src/Commands/RunPackage.command.js
@@ -1,8 +1,9 @@
-
+const { resolve } = require("path")
 
 const RunPackageCommand = async ({ args, startupParams, params }) => {
-   
-    
+    const { packagePath } = args
+    const absolutePackagePath = resolve(process.cwd(), packagePath)
+    console.log(absolutePackagePath)
 }
 
 module.exports = RunPackageCommand
